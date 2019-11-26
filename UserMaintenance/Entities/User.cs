376ead59d81083aafcfccf8ec0ce2f5f6a976721
Levente Csernus <kiscsernus98@gmail.com>
2019-11-26 14:57:19 +0100

    Adding resourcefile, changing UI

---
+++ UserMaintenance/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string firstname { get; set; } 
+        public string lastname { get; set; }
+        public string fullname
+        {
+            get
+            {
+                return string.Format("{0} {1}", lastname, firstname);
+            }
+        }
+    }
+}
