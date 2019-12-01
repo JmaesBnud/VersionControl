22da3f63b8ca1c5c7f043fc08df61aaf669b9822
Levente Csernus <kiscsernus98@gmail.com>
2019-12-01 14:44:14 +0000

    Saving funnction

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text = Resource1.FullName; // label1
             button1.Text = Resource1.Add; // button1
+            button2.Text = Resource1.WriteInFile;
 
 
             listBox1.DataSource = users;
@@ -36,5 +38,33 @@ namespace UserMaintenance
             };
             users.Add(u);
         }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.DefaultExt = "csv";
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.AddExtension = true;
+
+            
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+
+
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                {
+                    foreach (var s in users)
+                    {
+                        sw.Write(s.ID);
+                        sw.Write(";");
+                        sw.Write(s.fullname);
+                        sw.WriteLine();
+                    }
+                }
+        }
+        
     }
 }
